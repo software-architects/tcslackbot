a993e0b461f3de86676c50fe6ec83b5805d4ee62
not-matthias <26800596+not-matthias@users.noreply.github.com>
2019-05-26 16:55:25 +0200

    Fixed spelling

--- spec/test-spec.ts
+++ spec/test-spec.ts
@@ -1,11 +1,10 @@
-import { Handler } from 'express';
 import 'jasmine';
-// import { getEcho } from './../src/requests/echo-request';
 
 class MockedExpressService {
     public get(s: string, h: any) {
         return s + h;
     }
+
     public listen(port: number): string {
         return 'Listening on port ' + port;
     }
@@ -13,9 +12,8 @@ class MockedExpressService {
 
 // tslint:disable-next-line:max-classes-per-file
 class MockedEchoRequest {
-    public getEcho(): string {
-        const s = 'test';
-        return s;
+    public getEcho(message: string): string {
+        return message;
     }
 }
 
@@ -28,11 +26,12 @@ describe('Testing server', () => {
         echoRequest = new MockedEchoRequest();
     });
 
-    it('should call a mock get request', () => {
-        const newRequest: string = service.get('/api/echo/', echoRequest.getEcho());
+    it('should send a mock get request', () => {
+        const newRequest: string = service.get('/api/echo/', echoRequest.getEcho('test'));
         expect(newRequest).toBe('/api/echo/test');
     });
-    it('should start listen to a port', () => {
+
+    it('should start listening to port 8080', () => {
         const expressListen = service.listen(8080);
         expect(expressListen).toBe('Listening on port 8080');
     });
