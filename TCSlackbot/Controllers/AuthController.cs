78f6d30cd2183ba69c1fbcbead06619977fcd9de
not-matthias <26800596+not-matthias@users.noreply.github.com>
2019-10-26 11:54:58 +0200

    Updated the controller (keyvault works now)

--- TCSlackbot/Controllers/AuthController.cs
+++ TCSlackbot/Controllers/AuthController.cs
@@ -1,23 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace TCSlackbot.Controllers
 {
     [ApiController]
+    [Route("auth")]
     public class AuthController : ControllerBase
     {
         private readonly ILogger<AuthController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public AuthController(ILogger<AuthController> logger)
+        public AuthController(ILogger<AuthController> logger, IConfiguration config)
         {
             _logger = logger;
+            _configuration = config;
         }
 
-        [HttpPost]
-        [Route("auth")]
+        [HttpGet]
         public IActionResult Authenticate()
         {
-            return Ok("Hello World");
+            return Ok();
         }
     }
 }
