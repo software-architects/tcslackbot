6604b02c43e094435f72cd5c5ec26e4121334997
not-matthias <26800596+not-matthias@users.noreply.github.com>
2019-10-26 10:45:11 +0200

    Removed example code

---
+++ TCSlackbot/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TCSlackbot.Controllers
+{
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("auth")]
+        public IActionResult Authenticate()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
