7cd325b89b21441b404b12843060c4d0caf9f47c
not-matthias <26800596+not-matthias@users.noreply.github.com>
2019-10-31 13:02:08 +0100

    Added first example (https://github.com/openiddict/openiddict-samples/tree/dev/samples/CodeFlow)

--- TCSlackbot/Controllers/AuthController.cs
+++ TCSlackbot/Controllers/AuthController.cs
@@ -1,42 +1,64 @@
 ﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using TCSlackbot.Logic;
 
 namespace TCSlackbot.Controllers
 {
     [ApiController]
-    [Route("auth")]
     public class AuthController : ControllerBase
     {
         private readonly ILogger<AuthController> _logger;
         private readonly IConfiguration _configuration;
+        private readonly IHttpClientFactory _factory;
         private readonly ISecretManager _secretManager;
         private readonly IBotClient _botClient;
 
         private readonly SlackConfig _slackConfig;
 
-        public AuthController(ILogger<AuthController> logger, IConfiguration config, ISecretManager secretManager, IBotClient botClient, IOptions<SlackConfig> slackConfig)
+        public AuthController(ILogger<AuthController> logger, IConfiguration config, IHttpClientFactory factory, ISecretManager secretManager, IBotClient botClient, IOptions<SlackConfig> slackConfig)
         {
             _logger = logger;
             _configuration = config;
+            _factory = factory;
             _secretManager = secretManager;
             _botClient = botClient;
             _slackConfig = slackConfig.Value ?? throw new ArgumentException(nameof(SlackConfig));
         }
 
-        [HttpGet]
-        public async Task<IActionResult> Authenticate()
-        {
-            var accessToken = await HttpContext.GetTokenAsync("access_token");
-            Console.WriteLine(accessToken);
 
-            return Ok(_secretManager.GetSecret("mySecret"));
+        [HttpGet("~/signin")]
+        public ActionResult SignIn()
+        {
+            return Challenge(new AuthenticationProperties { RedirectUri = "/" }, OpenIdConnectDefaults.AuthenticationScheme);
         }
+
+
+        [HttpGet]
+        [Route("signin-oidc")]
+        public async Task<IActionResult> PostCallback()
+        {
+            var httpClient = _factory.CreateClient("APIClient");
+            var token = await HttpContext.GetTokenAsync(CookieAuthenticationDefaults.AuthenticationScheme, "access_token");
+            return Ok();
+        }
+
+        [Authorize, HttpPost("~/")]
+        public async Task<IActionResult> SomeFunction()
+        {
+            var httpClient = _factory.CreateClient("APIClient");
+            var token = await HttpContext.GetTokenAsync(CookieAuthenticationDefaults.AuthenticationScheme, "access_token");
+            return Ok();
+        }
+
 
         [HttpGet]
         [Route("startBot")]
--- TCSlackbot/Startup.cs
+++ TCSlackbot/Startup.cs
@@ -5,6 +5,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using TCSlackbot.Logic;
 
@@ -15,6 +18,8 @@ namespace TCSlackbot
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
         }
 
         public IConfiguration Configuration { get; }
@@ -27,29 +32,42 @@ namespace TCSlackbot
             var authenticationConfig = Configuration.GetSection("AuthenticationConfig").Get<AuthenticationConfig>();
 
             // https://github.com/onelogin/openid-connect-dotnet-core-sample
+            // https://github.com/KevinDockx/OpenIDConnectInDepth/blob/master/src/Sample.WebClient/Startup.cs
             services.AddAuthentication(options =>
                 {
                     options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
                 })
                 .AddCookie(options =>
                 {
-                    options.LoginPath = "/auth";
+                    options.LoginPath = "/signin-oidc";
                     options.AccessDeniedPath = "/error";
                 })
-                .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+                .AddOpenIdConnect(options =>
                 {
-                    options.Authority = "https://auth.timecockpit.com/connect/authorize";
-
                     options.ClientId = authenticationConfig.ClientId;
                     options.ClientSecret = authenticationConfig.ClientSecret;
-                    options.ResponseType = "code";
 
-                    options.SaveTokens = true;
+                    options.RequireHttpsMetadata = true;
                     options.GetClaimsFromUserInfoEndpoint = true;
+                    options.SaveTokens = true;
+
+                    // Use the authorization code flow.
+                    options.ResponseType = OpenIdConnectResponseType.Code;
+                    options.AuthenticationMethod = OpenIdConnectRedirectBehavior.RedirectGet;
+
+                    options.Authority = "https://auth.timecockpit.com";
 
                     options.Scope.Add("openid");
                     options.Scope.Add("offline_access");
+                    //options.Scope.Add("email");
+
+                    options.SecurityTokenValidator = new JwtSecurityTokenHandler
+                    {
+                        // Disable the built-in JWT claims mapping feature.
+                        InboundClaimTypeMap = new Dictionary<string, string>()
+                    };
+
+                    //options.TokenValidationParameters.NameClaimType = "name";
                 });
 
             services.AddTransient<ISecretManager, SecretManager>();
@@ -59,6 +77,11 @@ namespace TCSlackbot
             services.Configure<AuthenticationConfig>(Configuration.GetSection("AuthenticationConfig"));
 
             services.AddControllers();
+
+            services.AddHttpClient("APIClient", client =>
+            {
+                client.BaseAddress = new Uri("https://api.timecockpit.com");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -69,13 +92,13 @@ namespace TCSlackbot
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseAuthentication();
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
             app.UseAuthorization();
-
-            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
