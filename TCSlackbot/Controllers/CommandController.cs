468a57db0bc77a7d1dbe2fac6070c6558671909f
Patrick Wegerer <patrick.wegerer@gmail.com>
2019-11-07 09:34:21 +0100

    Added DataProtection to CommandController

--- TCSlackbot/Controllers/CommandController.cs
+++ TCSlackbot/Controllers/CommandController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,12 +8,19 @@ using System.Threading.Tasks;
 using TCSlackbot.Logic;
 
 namespace TCSlackbot.Controllers
-{   
-
+{
+    
     [ApiController]
     [Route("command")]
     public class CommandController : ControllerBase
     {
+        private readonly IDataProtector _protector;
+
+        public CommandController(IDataProtectionProvider provider)
+        {
+            _protector = provider.CreateProtector("UUIDProtector");
+        }
+
         [HttpPost]
         [Route("slashcommand")]
         public JsonResult HandleCommand([FromForm] SlackSlashCommand ssc)
@@ -24,6 +33,20 @@ namespace TCSlackbot.Controllers
         public IActionResult Ping([FromForm] SlackSlashCommand ssc)
         {
             return Ok("Pong");
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login([FromForm] SlackSlashCommand ssc)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                return Ok("https://ngroksomething.com/auth/link/?uuid=" + _protector.Protect(ssc.UserId));
+            } else
+            {
+                return Ok("https://tcslackbot.azurewebsites.net:6001/auth/link/?" + _protector.Protect(ssc.UserId));
+            }
+            
         }
     }
 }
