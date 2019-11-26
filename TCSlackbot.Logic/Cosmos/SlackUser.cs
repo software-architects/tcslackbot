162c0fa137829efec0a3f35b563a3670ba9c3e2d
not-matthias <26800596+not-matthias@users.noreply.github.com>
2019-11-26 09:49:04 +0100

    Cleanup

--- TCSlackbot.Logic/Cosmos/SlackUser.cs
+++ TCSlackbot.Logic/Cosmos/SlackUser.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Text.Json.Serialization;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace TCSlackbot.Logic
 {
     public class SlackUser
     {
-        [JsonPropertyName("id")]
+        [JsonProperty(PropertyName = "id")]
         public string UserId { get; set; }
+
         public DateTime StartTime { get; set; }
+
         public DateTime BreakTime { get; set; }
+
         public DateTime EndTime { get; set; }
+
         public bool OnBreak { get; set; } = false;
     }
 }
