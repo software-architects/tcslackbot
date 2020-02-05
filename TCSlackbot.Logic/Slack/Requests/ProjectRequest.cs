d7208b805e29000cf05dd168ab132ef6a3e729fb
Patrick Wegerer <patrick.wegerer@gmail.com>
2020-02-05 17:58:20 +0100

    Added a class for accepting IncomingProject

---
+++ TCSlackbot.Logic/Slack/Requests/ProjectRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace TCSlackbot.Logic.Slack.Requests
+{
+    public partial class ProjectRequest
+    {
+        // [JsonPropertyName("ok")]
+        // public bool Ok { get; set; }
+
+        [JsonPropertyName("value")]
+        public List<ProjectValues> Values { get; set; }
+    }
+
+    public partial class ProjectValues
+    {
+        [JsonPropertyName("APP_Code")]
+        public string ProjectName { get; set; }
+
+        // [JsonPropertyName("user")]
+        // public string User { get; set; }
+    }
+}
