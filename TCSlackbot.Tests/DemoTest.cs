a0bd8847da44672fbed0bdffc0b556b674caf6a2
not-matthias <26800596+not-matthias@users.noreply.github.com>
2020-04-11 20:49:58 +0200

    Added another test

---
+++ TCSlackbot.Tests/DemoTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TCSlackbot.Tests
+{
+    public class DemoTest
+    {
+        [Fact]
+        public void SomeTest()
+        {
+            Assert.True(false);
+        }
+    }
+}
